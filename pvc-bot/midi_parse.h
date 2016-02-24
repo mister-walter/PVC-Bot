965ba7814d8842c4b97b5aaa233d20ca71f58a1d
Tanuj Sane <tsane@wpi.edu>
2016-02-24 18:52:02 -0500

    Started midi_parse library

---
+++ pvc-bot/midi_parse.h
@@ -0,0 +1,13 @@
+/** midi_parse.h - Library for parsing MIDI input over serial
+ *  
+ *  @author Tanuj Sane
+ */
+#define NOTE_OFF 0x10
+#define NOTE_ON 0x90
+#define AFTERTOUCH 0xA0
+#define CMD 0xF0
+
+void midi_in(){
+  
+}
+
