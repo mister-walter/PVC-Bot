65592149c64845524262821bbf97e7b8b764ef52
Tanuj Sane <tsane@wpi.edu>
2016-02-26 01:03:45 -0500

    Removed unnecessary library, consolidated to .ino file

--- pvc-bot/pvc-bot.ino
+++ pvc-bot/pvc-bot.ino
@@ -2,7 +2,30 @@
  *  @author Tanuj Sane
  */
 
-#include "midi_2_music.h"
+// circuit driver constants
+#define CLK 5
+#define EN  6
+#define RST 7
+#define S1  0
+
+ // command bytes //TODO: SET CHANNEL USING LOW-ORDER NIBBLE
+#define NOTE_OFF 0x10
+#define NOTE_ON 0x90
+
+// Function declarations
+void init_timer();
+void play_note();
+void _do_nothing();
+ISR(TIMER1_COMPA_vect);
+void set(int b);
+void clear();
+void config();
+
+// variables
+byte cmd, pitch;
+byte off; // velocity replaced by on or off flag
+
+int t12 = 0x0001; int t13 = 0x0001;
 
 void setup() {
   init_timer();
@@ -12,3 +35,5 @@ void setup() {
 void loop() {
   
 }
+
+
